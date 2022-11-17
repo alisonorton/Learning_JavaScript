05a688198507c318c99cee144be12ec3d6903a6b
alisonorton <orton.alison@gmail.com>
2022-11-16 20:39:45 -1000

    Adding reset button

--- EventListeners/script.js
+++ EventListeners/script.js
@@ -10,8 +10,13 @@
 li.addEventListener('click',(event) => {
     console.log(event)
 }*/
+//Function to generate random color
+const randColor = () => {
+    let hexNum = Math.floor(Math.random() * 16777215).toString(16);
+    return hexNum;
+};
 
-
+//Adding red outline when mouse is on grid
 const gridContainer = document.querySelector(".grid");
 
 gridContainer.addEventListener("mouseenter", () => {
@@ -24,9 +29,11 @@ gridContainer.addEventListener("mouseleave", () => {
   
 });
 
+
 const cell = document.querySelectorAll(".cell");
 
 cell.forEach((cell) =>{
+    //Adding outline when mouse is hovering over cell
     cell.addEventListener("mouseenter", () => {
         cell.style.outline = "2px solid green";
     });
@@ -34,8 +41,47 @@ cell.forEach((cell) =>{
     cell.addEventListener("mouseleave", () => {
         cell.style.outline = "";
     });
-
+    //Generating random color when a cell is clicked
     cell.addEventListener("click", () => {
-        cell.style.backgroundColor = "blue";
+        if(cell.style.backgroundColor) {
+            cell.style.backgroundColor = "";
+            console.log("NO color");
+        }
+        else {
+            cell.style.backgroundColor = `#${randColor()}`;
+            console.log("Printed color");
+        }
+        
     })
 }) 
+
+//Changing background color when spacebar is pressed
+const body = document.body;
+
+body.addEventListener("keydown", (e) => {
+    let highLighted = false;
+
+
+    if(e.code === "Space"){
+        if (body.style.backgroundColor){
+            body.style.backgroundColor = ""
+        }
+        else {
+            body.style.backgroundColor = "grey"
+        }
+        
+    }
+})
+
+//Reset button
+const button = document.querySelector(".button");
+
+button.addEventListener("click", () =>{
+    cell.forEach((cell) =>{
+        if (cell.style.backgroundColor){
+            cell.style.backgroundColor = "";
+        }
+    })
+        body.style.backgroundColor = ""
+    
+})
