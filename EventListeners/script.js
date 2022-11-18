713e030601041f4b364f873bf51b2e3f55f157db
alisonorton <orton.alison@gmail.com>
2022-11-18 11:31:44 -1000

    Editing comments

--- EventListeners/script.js
+++ EventListeners/script.js
@@ -1,15 +1,5 @@
-/**
- * Practice: Play with event listeners
- * - Use an event listener and CSS either inline or through an added class to draw a highlight around the entire grid when you hover over it with your mouse.
- * - Add an event listener to each grid cell to highlight that cell when you hover your mouse over it.
- * - Add an event listener to each grid cell to change its background color when it is clicked.
- * - Add an event listener to a specific key on the keyboard to change the background color of the whole page - from dark to light and back again.
- */
 
-/*const li = ul.querySelector(".grid");
-li.addEventListener('click',(event) => {
-    console.log(event)
-}*/
+
 //Function to generate random color
 const randColor = () => {
     let hexNum = Math.floor(Math.random() * 16777215).toString(16);
