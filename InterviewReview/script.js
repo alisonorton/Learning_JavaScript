9f9e0c3df2873781bddad7073c389a0704dbce6a
alisonorton <orton.alison@gmail.com>
2022-12-01 10:52:04 -1000

    Fixed user input bug

--- InterviewReview/script.js
+++ InterviewReview/script.js
@@ -1,23 +1,24 @@
 
 const button = document.getElementById("button");
 const main = document.querySelector("main");
-
-
-let number = "";
+const displayDivs = document.getElementById("display")
 
 function display(num){
     for(let i = 1; i <= num; i++){
         console.log("in the loop " + i);
-        number += `<div>${i}</div>`;
+        let circle = document.createElement("div")
+        circle.classList.add("circle")
+        let num = document.createElement("p")
+        num.textContent = i
+        circle.appendChild(num)
+        displayDivs.appendChild(circle)
+
     }
 }
 
-display(9)
 
 button.addEventListener('click', () => {
     let input = parseInt(document.getElementById("input").value);
     console.log(input);
     display(input);
 });
-
-document.getElementById("display").innerHTML = number;
